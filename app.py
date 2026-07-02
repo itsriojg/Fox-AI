e70d5c8182a4952705398b21ebe90f2c32d985f3
rioooo <riog2911@gmail.com>
2026-07-02 15:56:09 +0700

    menambahkan route post

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    message = request.args.get("message", "")
+    message = request.form.get("message", "")
     return render_template("index.html", nama="Rio", message=message)
 
 app.run()
