022c48fa742764824f59e2f2aa9795c09d4138ec
rioooo <riog2911@gmail.com>
2026-07-04 08:14:07 +0700

    menambahkan dictionary yang awalnya if elif else, dengan hasil chatbot sederhana

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    message = request.form.get("message", "")
-    return render_template("index.html", nama="Rio", message=message)
+    message = request.form.get("pesan", "")
+    message = message.strip().lower()
+    responses = {
+      "halo" : "Halo juga:)",
+      "kamu siapa" : "Saya project pillow-fox",
+      "bisa bantu saya?" : "Tentu saja, apa yang bisa saya bantu?",
+      "selamat pagi" : "Selamat pagi",
+      "" : ""
+    }
+    reply = responses.get(message, "Maaf saya tidak mengerti")
+    return render_template("index.html", nama="Rio", pesan=message, balasan=reply)
 
 app.run()
