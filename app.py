f169666540f4a0532672be7d31d2acc2e00c63ef
rioooo <riog2911@gmail.com>
2026-07-04 08:26:05 +0700

    mengeluarkan dictionary responses dari dalam fungsi, karna bersifat data konstan

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
-def home():
-    message = request.form.get("pesan", "")
-    message = message.strip().lower()
-    responses = {
+responses = {
       "halo" : "Halo juga:)",
       "kamu siapa" : "Saya project pillow-fox",
       "bisa bantu saya?" : "Tentu saja, apa yang bisa saya bantu?",
       "selamat pagi" : "Selamat pagi",
       "" : ""
     }
+@app.route("/", methods=["GET", "POST"])
+def home():
+    message = request.form.get("pesan", "")
+    message = message.strip().lower()
     reply = responses.get(message, "Maaf saya tidak mengerti")
     return render_template("index.html", nama="Rio", pesan=message, balasan=reply)
 
