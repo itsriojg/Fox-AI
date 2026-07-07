7cb3d16b527769b9030e543ffbd100561bd3ce4e
rioooo <riog2911@gmail.com>
2026-07-07 21:12:23 +0700

    menambah program cookie & session

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect 
 from chatbot import get_reply
-from history import history, tambah_message
+from history import tambah_message, ambil_history
 app = Flask(__name__)
+app.secret_key = "rahasiaaa"
 
 @app.route("/", methods=["GET", "POST"])
 
 def home():
-  reply = ""
   if request.method == "POST":
     message = request.form.get("pesan", "")
     if message != "":
       reply = get_reply(message)
       tambah_message("Rio", message)
       tambah_message("AI", reply)
-  return render_template("index.html", nama="RIO", riwayat_chat=history)
+      return redirect("/")
+  return render_template("index.html", nama="RIO", riwayat_chat=ambil_history())
 
 app.run()
 
--- history.py
+++ history.py
@@ -1,9 +1,17 @@
-history = []
+from flask import session
 
 def tambah_message(sender, text):
+  if "history" not in session:
+    session["history"] = []
   pesan = {
     "sender": sender,
     "text": text
   }
-  history.append(pesan)
+  session["history"].append(pesan)
+  session.modified = True
   
+def ambil_history():
+  if "history" not in session:
+    session["history"] = []
+  return session["history"]
+ 
